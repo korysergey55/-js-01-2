f4a94197a14a3d5b1e3a0f3f2280ce49ec7de84d
Serhiy Korytskyy <korysergey55@gmail.com>
2021-04-10 21:05:44 +0300

    first commit

---
+++ js/js-01-2-switch.js
@@ -0,0 +1,24 @@
+//  Количество звезд отеля и цена
+
+const stars = 0;
+let price;
+
+switch(stars){
+    case 1:
+    case 2:
+    case 3:
+        price = 25;
+        break;
+    case 4:
+        price = 40;
+        break;
+    case 5:
+        price = 60;
+        break;
+    default:
+        console.log('Enter quantity of stars please!');
+     
+}
+console.log(`Room in this hotel cost - ${price}$.`);
+
+
