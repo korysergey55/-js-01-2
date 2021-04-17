ac6397d0371b43d795a006639ab8e7b86350d042
Serhiy Korytskyy <korysergey55@gmail.com>
2021-04-17 15:57:50 +0300

    add-event-alister

--- js/js-08-button-event-elister.js
+++ js/js-08-button-event-elister.js
@@ -1,18 +1,20 @@
 
 const btnAdd = document.querySelector('button[data-value]');
 const input = document.querySelector('input[data-value]');
+const output = document.querySelector('.js-outputs');
+
 
 let total = 0;
 
 btnAdd.addEventListener('click', function(){
   
-  console.log(input.value);
+  const valueInput = Number(input.value);// input всегда возвращает строку
 
-  const valueInput = Number(input.value);
-  
   console.log(valueInput);
   console.log(typeof(valueInput));
 
   total += valueInput;
-  console.log(total);
+  output.textContent = total;
+
+
 });
